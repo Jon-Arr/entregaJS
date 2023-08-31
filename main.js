4821a2c18022c40f28fe18e97d92faca60e7b6a5
Jonathan Arriaza <jaarriaza.e@gmail.com>
2023-08-30 22:47:05 -0400

    entrega 2

--- main.js
+++ main.js
@@ -1,60 +1,82 @@
-//
-let op = ""
-let numA = ""
-let numB = ""
+const alumnos = ["jonathan", "barbara"]
 
-function operacion() {
+function promedio() {
+    let bienvenida = prompt("Bienvenid@, ingrese 'nuevo' si desea ingresar un nuevo alumno, 'notas' para insertar las notas y obtener su promedio o 'alumnos' para ver la lista de alumnos:")
 
-    alert("Bienvenido, a continuacion podra realizar operaciones matematicas basicas")
-    let op = prompt("Desea: (s)sumar - (r)restar - (m)multiplicar - (d)dividir")
-
-    if (op == "s" || op == "r" || op == "m" || op == "d") {
-
-        let numA = Number(prompt("Ingrese un numero"))
-        let numB = Number(prompt("Ingrese otro numero"))
-
-        if (numA == "" || numB == "" || numA == undefined || numB == undefined) {
-            alert("No ingreso ningun numero")
-            operacion(op, numA, numB)
+    if (bienvenida == "nuevo") {
+        let nuevo = prompt("Igrese su nombre en minusculas para añadirlo a los alumnos o escriba 'esc' para volver al menu")
+        if (nuevo == "esc") {
+            alert("Volviendo al menu principal")
+            promedio()
+        }
+        else if (nuevo == "" || nuevo == undefined) {
+            alert("No ingreso un dato valido o esta vacio")
+            promedio()
         }
         else {
+            alumnos.push(nuevo)
+            alert("Alumno insertado correctamente")
+            promedio()
+        }
+    }
+    else if (bienvenida == "notas") {
 
-            switch (op) {
-                case "s":
-                    res = numA + numB
-                    alert("El resultado es: "+res)
-                    break;
+        function notas() {
 
-                case "r":
-                    res = numA - numB
-                    alert("El resultado es: "+res)
-                    break;
+            let nombre = prompt("Ingrese nombre del alumno en minusculas o escriba 'esc' para volver al menu")
+            let compNombre = alumnos.some((el) => el == nombre)
 
-                case "m":
-                    res = numA * numB
-                    alert("El resultado es: "+res)
-                    break;
+            if (nombre == "esc") {
+                alert("Volviendo al menu principal")
+                promedio()
+            }
+            else if (nombre == "" || nombre == undefined || compNombre == false) {
+                alert("No ingreso un dato valido o Usuario no encontrado")
+                notas()
+            }
+            else {
+                alert("Bienvenid@, " + nombre)
+                const listaNotas = []
+                let sumaNotas = 0
 
-                case "d":
-                    res = numA / numB
-                    alert("El resultado es: "+res)
-                    break;
+                let primerNota = Number(prompt("Ingrese nota 1 de 5"))
+                listaNotas.push(primerNota)
+                let segundaNota = Number(prompt("Ingrese nota 2 de 5"))
+                listaNotas.push(segundaNota)
+                let tercerNota = Number(prompt("Ingrese nota 3 de 5"))
+                listaNotas.push(tercerNota)
+                let cuartaNota = Number(prompt("Ingrese nota 4 de 5"))
+                listaNotas.push(cuartaNota)
+                let quintaNota = Number(prompt("Ingrese nota 5 de 5"))
+                listaNotas.push(quintaNota)
 
-                default:
-                    alert("Error en el ingreso de datos")
-                    break;
+                sumaNotas = listaNotas.reduce((a, b) => a + b, 0)
+                sumaNotas = sumaNotas / listaNotas.length
+                alert(nombre + " su promedio de notas es: " + sumaNotas)
             }
         }
+        notas()
 
     }
-    else if(op == undefined){
-        alert("No ingreso una operacion")
-        operacion(op, numA, numB)
+    else if (bienvenida == "alumnos") {
+        let contAlumnos = ""
+
+        for (i = 0; i < alumnos.length; i++) {
+            contAlumnos = "Alumno - " + alumnos[i] + ". \n" + contAlumnos
+        }
+        alert(contAlumnos)
+        promedio()
+    }
+    else if (bienvenida == "" | bienvenida == undefined) {
+        alert("No se ingreso informacion")
+        promedio()
     }
     else {
-        alert("Ingrese una opcion valida")
-        operacion(op, numA, numB)
+        alert("Ingrese la informacion solicitada")
+        promedio()
     }
-}
 
-operacion(op, numA, numB)
+    promedio()
+
+}
+promedio()
