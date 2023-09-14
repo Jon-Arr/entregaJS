752b6ed5d2d5c318bddf3fdba79b5e223dc812fe
Jonathan Arriaza <jaarriaza.e@gmail.com>
2023-09-13 23:06:04 -0300

    Cambio de prompt y alert a html y SwAl

--- main.js
+++ main.js
@@ -1,82 +1,76 @@
-const alumnos = ["jonathan", "barbara"]
-
-function promedio() {
-    let bienvenida = prompt("Bienvenid@, ingrese 'nuevo' si desea ingresar un nuevo alumno, 'notas' para insertar las notas y obtener su promedio o 'alumnos' para ver la lista de alumnos:")
-
-    if (bienvenida == "nuevo") {
-        let nuevo = prompt("Igrese su nombre en minusculas para añadirlo a los alumnos o escriba 'esc' para volver al menu")
-        if (nuevo == "esc") {
-            alert("Volviendo al menu principal")
-            promedio()
-        }
-        else if (nuevo == "" || nuevo == undefined) {
-            alert("No ingreso un dato valido o esta vacio")
-            promedio()
-        }
-        else {
-            alumnos.push(nuevo)
-            alert("Alumno insertado correctamente")
-            promedio()
+class Alumnos {
+    constructor() {
+        this.lista = []
+    }
+    agregarAlumno(nombre) {
+        this.lista.push(nombre)
+        localStorage.setItem("alumnos", JSON.stringify(this.lista))
+    }
+    calcularPromedio(nombre, nota1, nota2, nota3) {
+        const promedio = (nota1 + nota2 + nota3) / 3
+        return {
+            nombre,
+            promedio,
         }
     }
-    else if (bienvenida == "notas") {
-
-        function notas() {
-
-            let nombre = prompt("Ingrese nombre del alumno en minusculas o escriba 'esc' para volver al menu")
-            let compNombre = alumnos.some((el) => el == nombre)
-
-            if (nombre == "esc") {
-                alert("Volviendo al menu principal")
-                promedio()
-            }
-            else if (nombre == "" || nombre == undefined || compNombre == false) {
-                alert("No ingreso un dato valido o Usuario no encontrado")
-                notas()
-            }
-            else {
-                alert("Bienvenid@, " + nombre)
-                const listaNotas = []
-                let sumaNotas = 0
-
-                let primerNota = Number(prompt("Ingrese nota 1 de 5"))
-                listaNotas.push(primerNota)
-                let segundaNota = Number(prompt("Ingrese nota 2 de 5"))
-                listaNotas.push(segundaNota)
-                let tercerNota = Number(prompt("Ingrese nota 3 de 5"))
-                listaNotas.push(tercerNota)
-                let cuartaNota = Number(prompt("Ingrese nota 4 de 5"))
-                listaNotas.push(cuartaNota)
-                let quintaNota = Number(prompt("Ingrese nota 5 de 5"))
-                listaNotas.push(quintaNota)
-
-                sumaNotas = listaNotas.reduce((a, b) => a + b, 0)
-                sumaNotas = sumaNotas / listaNotas.length
-                alert(nombre + " su promedio de notas es: " + sumaNotas)
-            }
-        }
-        notas()
-
+    obtenerLista() {
+        return this.lista
     }
-    else if (bienvenida == "alumnos") {
-        let contAlumnos = ""
-
-        for (i = 0; i < alumnos.length; i++) {
-            contAlumnos = "Alumno - " + alumnos[i] + ". \n" + contAlumnos
-        }
-        alert(contAlumnos)
-        promedio()
-    }
-    else if (bienvenida == "" | bienvenida == undefined) {
-        alert("No se ingreso informacion")
-        promedio()
-    }
-    else {
-        alert("Ingrese la informacion solicitada")
-        promedio()
-    }
-
-    promedio()
-
 }
-promedio()
+const alumnos = new Alumnos()
+
+const btn_nuevo = document.getElementById("btn_nuevo")
+btn_nuevo.addEventListener("click", () => {
+    Swal.fire({
+        title: "<h2>Ingrese su nombre en minúsculas para añadirlo a los alumnos</h2>",
+        html: `<input type="text" id="new_nombre" class="swal2-input" placeholder="Nombre">`,
+        confirmButtonText: 'Ingresar',
+        focusConfirm: false,
+        preConfirm: () => {
+            const new_nombre = Swal.getPopup().querySelector('#new_nombre').value
+            if (!new_nombre) {
+                Swal.showValidationMessage('Inserte un nombre')
+            }
+            return { new_nombre }
+        }
+    }).then((result) => {
+        const nombre = result.value.new_nombre
+        alumnos.agregarAlumno(nombre)
+
+        Swal.fire(`Bienvenid@: ${nombre}`)
+    })
+})
+
+const btn_notas = document.getElementById("btn_notas")
+btn_notas.addEventListener("click", () => {
+    Swal.fire({
+        title: "<h2>Ingrese nombre del alumno en minúsculas</h2>",
+        html: `<input type="text" id="new_nombre" class="swal2-input" placeholder="Nombre">
+             <input type="number" id="new_nota1" class="swal2-input" placeholder="0-10">
+             <input type="number" id="new_nota2" class="swal2-input" placeholder="0-10">
+             <input type="number" id="new_nota3" class="swal2-input" placeholder="0-10">`,
+        confirmButtonText: 'Promedio',
+        focusConfirm: false,
+        preConfirm: () => {
+            const new_nombre = Swal.getPopup().querySelector('#new_nombre').value
+            const new_nota1 = parseFloat(Swal.getPopup().querySelector('#new_nota1').value)
+            const new_nota2 = parseFloat(Swal.getPopup().querySelector('#new_nota2').value)
+            const new_nota3 = parseFloat(Swal.getPopup().querySelector('#new_nota3').value)
+            if (!new_nombre || isNaN(new_nota1) || isNaN(new_nota2) || isNaN(new_nota3)) {
+                Swal.showValidationMessage('Inserte su nombre y sus notas')
+            }
+            return { new_nombre, new_nota1, new_nota2, new_nota3 }
+        }
+    }).then((result) => {
+        const { new_nombre, new_nota1, new_nota2, new_nota3 } = result.value
+        const promedio = alumnos.calcularPromedio(new_nombre, new_nota1, new_nota2, new_nota3)
+
+        Swal.fire(`Nombre: ${promedio.nombre}\nPromedio: ${promedio.promedio}`)
+    })
+})
+
+const btn_alumnos = document.getElementById("btn_alumnos")
+btn_alumnos.addEventListener("click", () => {
+    const listaAlumnos = alumnos.obtenerLista().join(". \n")
+    Swal.fire(listaAlumnos)
+})  
